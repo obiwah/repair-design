d89f92bc5662b7605013afd41099e557ec701c5a
obiwah <a.v.kuz@inbox.ru>
2019-12-09 16:47:52 +0700

    upd gulpfile.js, upd  index.html (add header)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
-const gulp = require("gulp");
+const gulp = require("gulp"),
+	browserSync = require('browser-sync').create();
 
 gulp.task('hello', (done) => {
 	console.log('hello world');
 	done();
 });
+
+// Static server
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+	gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+
