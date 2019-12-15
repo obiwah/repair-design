28aeab7f7bb13db93d7011ba3ebca6a601bc6922
obiwah <a.v.kuz@inbox.ru>
2019-12-16 04:22:08 +0700

    refact: add js for closing modal w. esc and outclick

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@ document.addEventListener(`DOMContentLoaded`, () => {
 	const modal =  document.querySelector('.modal'),
 		modalBtn = document.querySelectorAll(`[data-toggle=modal]`),
 		closeBtn = document.querySelector('.modal__close'),
-
-		switchModal = () => modal.classList.toggle(`modal--visible`);
+		modalClose = () => modal.classList.remove(`modal--visible`);
 
 	modalBtn.forEach(element => {
-		element.addEventListener(`click`, switchModal);
+		element.addEventListener(`click`, () => {
+			modal.classList.add(`modal--visible`);
+
+			modal.addEventListener(`click`, (clickEvent) => {
+				if (clickEvent.target.classList[0] === `modal`) modalClose();
+			});
+
+			document.addEventListener(`keydown`, (keyEvent) => {
+				if (keyEvent.key === `Escape`) modalClose();
+			});
+		});
 	});
 
-	closeBtn.addEventListener(`click`, switchModal);
+	closeBtn.addEventListener(`click`, modalClose);
 });
