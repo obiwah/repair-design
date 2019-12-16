df2962ee19c0c0d0ce15e166a56b095311592582
obiwah <a.v.kuz@inbox.ru>
2019-12-16 13:06:09 +0700

    refact: refact main.js w. JQuery

--- js/main.js
+++ js/main.js
@@ -1,23 +1,45 @@
+// native JS version
+//
+// document.addEventListener(`DOMContentLoaded`, () => {
+// 	const modal =  document.querySelector('.modal'),
+// 		modalBtn = document.querySelectorAll(`[data-toggle=modal]`),
+// 		closeBtn = document.querySelector('.modal__close'),
+// 		modalClose = () => modal.classList.remove(`modal--visible`);
+//
+// 	modalBtn.forEach(element => {
+// 		element.addEventListener(`click`, () => {
+// 			modal.classList.add(`modal--visible`);
+//
+// 			modal.addEventListener(`click`, (clickEvent) => {
+// 				if (clickEvent.target.classList[0] === `modal`) modalClose();
+// 			});
+//
+// 			document.addEventListener(`keydown`, (keyEvent) => {
+// 				if (keyEvent.key === `Escape`) modalClose();
+// 			});
+// 		});
+// 	});
+//
+// 	closeBtn.addEventListener(`click`, modalClose);
+// });
+
 $(function () {
-	const modal = $(`.modal`),
-		modalBtn = document.querySelectorAll(`[data-toggle=modal]`),
-		closeBtn = document.querySelector('.modal__close'),
-		modalClose = () => modal.classList.remove(`modal--visible`);
-	console.log(modal);
+	const modal =  $(`.modal`),
+		modalBtn = $(`[data-toggle=modal]`),
+		closeBtn = $(`.modal__close`),
+		modalClose = () => modal.removeClass(`modal--visible`);
 
-	modalBtn.forEach(element => {
-		element.addEventListener(`click`, () => {
-			modal.classList.add(`modal--visible`);
+	modalBtn.click(function() {
+		modal.addClass(`modal--visible`);
 
-			modal.addEventListener(`click`, (clickEvent) => {
-				if (clickEvent.target.classList[0] === `modal`) modalClose();
-			});
+		modal.click((clickEvent) => {
+			if (clickEvent.target.classList[0] === `modal`) modalClose();
+		});
 
-			document.addEventListener(`keydown`, (keyEvent) => {
-				if (keyEvent.key === `Escape`) modalClose();
-			});
+		$(document).keydown((keyEvent) => {
+			if (keyEvent.key === `Escape`) modalClose();
 		});
 	});
 
-	closeBtn.addEventListener(`click`, modalClose);
-})
+	closeBtn.click(modalClose);
+});
