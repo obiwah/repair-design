09ed55d1b8ef947a6fcb7dd5fdab3978bb176c9c
obiwah <a.v.kuz@inbox.ru>
2019-12-30 00:46:57 +0700

    pillow

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,50 @@
-const {src, dest, watch} = require("gulp"),
-	browserSync = require('browser-sync').create(),
-	// cssmin = require('gulp-cssmin'),
-	// rename = require('gulp-rename'),
-	sass = require('gulp-sass');
+const {src, dest, watch, series} = require(`gulp`),
+	browserSync = require(`browser-sync`).create(),
+	sass = require(`gulp-sass`),
+	autoprefixer = require(`gulp-autoprefixer`),
+	cleanCss = require('gulp-clean-css');
 
 // Static server
 function bs() {
 	serveSass();
 	browserSync.init({
 		server: {
-			baseDir: "./"
+			baseDir: `./`
 		}
 	});
-	watch("./*.html").on('change', browserSync.reload);
-	watch(".sass/**/*.sass", serveSass);
-	watch(".sass/**/*.scss", serveSass);
-	watch("./js/*.js").on('change', browserSync.reload);
+	watch(`./*.html`).on(`change`, browserSync.reload);
+	watch(`.sass/**/*.sass`, serveSass);
+	watch(`.sass/**/*.scss`, serveSass);
+	watch(`./js/*.js`).on(`change`, browserSync.reload);
 }
 
 function serveSass() {
-	return src("./sass/**/*.sass", "./sass/**/*.scss")
+	return src(`./sass/**/*.sass`, `./sass/**/*.scss`)
 		.pipe(sass())
-		.pipe(dest("./css"))
+		.pipe(dest(`./css`))
 		.pipe(browserSync.stream());
 }
 
-// npm install --save-dev gulp-cssmin
+function buildHtml () {
+	return src(`*.html`)
+				.pipe(dest(`dist/`));
+}
 
-// gulp.task('minify', function (done) {
-// 	gulp.src('css/style.css')
-// 		.pipe(cssmin())
-// 		.pipe(rename({suffix: '.min'}))
-// 		.pipe(gulp.dest('css'));
-// 	done();
-// });
+function buildPhp() {
+	return src(`*.php`)
+		.pipe(dest(`dist/`))
+		.pipe(src(`phpMailer/*.php`))
+		.pipe(dest(`dist/phpMailer/`))
+}
+
+function buildCss() {
+	return src(`css/**/*.css`)
+		.pipe(autoprefixer({ cascade: false }))
+		.pipe(dest(`dist/css`))
+		.pipe(src([`dist/css/**/*.css`, `!dist/css/**/*.min.css`]))
+		.pipe(cleanCss({compatibility: 'ie8'}))
+}
+
 
 exports.serve = bs;
+exports.build = series(buildHtml, buildPhp, buildCss);
