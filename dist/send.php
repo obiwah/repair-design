e1f549f40b5ca8fb454fabb03be6ca861eb16c2a
obiwah <a.v.kuz@inbox.ru>
2020-01-09 05:32:47 +0700

    dist

---
+++ dist/send.php
@@ -0,0 +1,51 @@
+<?php
+
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userPhone = $_POST['userPhone'];
+$userQuestion = $_POST['userQuestion'];
+
+// Load Composer's autoloader
+require 'phpMailer/PHPMailer.php';
+require 'phpMailer/SMTP.php';
+require 'phpMailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+$mail->CharSet = 'UTF-8';
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'alexey.v.kuzn@gmail.com';                     // SMTP username
+    $mail->Password   = 'bOGLKH0cGylUGhR4hISA';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('alexey.v.kuzn@gmail.com');
+    $mail->addAddress('a.v.kuz@inbox.ru');     // Add a recipient
+
+    $mail->setLanguage('ru', 'phpmailer.lang-ru.php');
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}. Вопрос:
+      '${userQuestion}'";
+
+    if ($mail->send()) {
+      echo "Форма успешно отправлена";
+    } else {
+      echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    }
+
+} catch (Exception $e) {
+  echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
+
+?>
